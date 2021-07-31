8a050a03e02d33ab62529e8058715f515b20d614
Leanid Nasanovich <olen4ixxx@gmail.com>
2021-08-01 00:41:32 +0300

    js

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-alert('Продолжая использовать данный сайт, вы соглашаетесь, что я красавчик')
+// alert('Продолжая использовать данный сайт, вы соглашаетесь, что я красавчик')
+alert('By continuing to use the site, you agree that I am а dreamboat')
+const links = document.getElementById('links')
+
+links.style.color = 'blue'
+links.style.textAlign = 'center'
+// links.style.backgroundColor = 'yellow'
+
+
+
