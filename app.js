4cd79adf3c166268ca4641d47cb66dbb193c0e6b
Leanid Nasanovich <olen4ixxx@gmail.com>
2021-08-05 12:45:27 +0300

    js

--- app.js
+++ app.js
@@ -6,13 +6,16 @@
 // links.style.textAlign = 'center'
 // links.style.backgroundColor = 'yellow'
 document.body.style.backgroundImage = "url('img/bg1.gif')"
+document.body.style.backgroundSize = "cover"
+// document.body.style.backgroundSize = `${window.innerHeight/718*120}%`
+document.body.style.backgroundRepeat = no-repeat
 // let percentage = window.innerHeight/718
 function detectMob() {
     return ((window.innerWidth < window.innerHeight))
 }
 if (detectMob()){
     document.body.style.backgroundImage = "url('img/bg1.gif')"
-    document.body.style.backgroundSize = `${window.innerHeight/718*150}%`
+    document.body.style.backgroundSize = `${window.innerHeight/718*120}%`
     document.body.style.backgroundRepeat = no-repeat
 }
 // let detect = new MobileDetect(window.navigator.userAgent)
